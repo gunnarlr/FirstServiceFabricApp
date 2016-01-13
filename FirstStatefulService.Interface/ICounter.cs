24fddd2adf2d45f4275f4d17833e55d6cd8b016d
Gunnar L. Rasmussen <gunnarlr@outlook.com>
2016-01-13 15:12:16 +0100

    Web service and interface. Soon to communicate.

---
+++ FirstStatefulService.Interface/ICounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStatefulService.Interface
+{
+    using Microsoft.ServiceFabric.Services.Remoting;
+
+    public interface ICounter: IService
+    {
+        Task<long> GetCountAsync();
+    }
+}
--- FirstStatefulService/FirstStatefulService.cs
+++ FirstStatefulService/FirstStatefulService.cs
@@ -6,22 +6,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using FirstStatefulService.Interface;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 
 namespace FirstStatefulService
 {
     /// <summary>
     /// The FabricRuntime creates an instance of this class for each service type instance.
     /// </summary>
-    internal sealed class FirstStatefulService : StatefulService
+    internal sealed class FirstStatefulService : StatefulService, ICounter
     {
+        private static string theCountsDictionaryName = "myDictionary";
+        private static string theCountsKey = "Counter-1";
+
+        public async Task<long> GetCountAsync()
+        {
+            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>(theCountsDictionaryName);
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var result = await myDictionary.TryGetValueAsync(tx, theCountsKey);
+                return result.HasValue ? result.Value : 0;
+            }
+        }
+
         /// <summary>
         /// Optional override to create listeners (like tcp, http) for this service replica.
         /// </summary>
         /// <returns>The collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
+            return new List<ServiceReplicaListener>()
+            {
+                new ServiceReplicaListener((initparams) =>
+                    new ServiceRemotingListener<ICounter>(initparams, this))
+            };
+
             // TODO: If your service needs to handle user requests, return a list of ServiceReplicaListeners here.
-            return new ServiceReplicaListener[0];
+            // return new ServiceReplicaListener[0];
         }
 
         /// <summary>
@@ -34,7 +55,7 @@ namespace FirstStatefulService
             // TODO: Replace the following sample code with your own logic.
 
             // Gets (or creates) a replicated dictionary called "myDictionary" in this partition.
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>(theCountsDictionaryName);
 
             // This partition's replica continues processing until the replica is terminated.
             while (!cancelServicePartitionReplica.IsCancellationRequested)
@@ -45,7 +66,7 @@ namespace FirstStatefulService
                 {
 
                     // Try to read a value from the dictionary whose key is "Counter-1".
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter-1");
+                    var result = await myDictionary.TryGetValueAsync(tx, theCountsKey);
 
                     // Log whether the value existed or not.
                     ServiceEventSource.Current.ServiceMessage(this, "Current Counter Value: {0}",
@@ -53,7 +74,7 @@ namespace FirstStatefulService
 
                     // If the "Counter-1" key doesn't exist, set its value to 0
                     // else add 1 to its current value.
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter-1", 0, (k, v) => ++v);
+                    await myDictionary.AddOrUpdateAsync(tx, theCountsKey, 0, (k, v) => ++v);
 
                     // Committing the transaction serializes the changes and writes them to this partition's secondary replicas.
                     // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
