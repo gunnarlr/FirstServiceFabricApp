a1f3f363d1c68cd8050f8fddb051680956576b50
Gunnar L. Rasmussen <gunnarlr@outlook.com>
2016-01-15 11:44:21 +0100

    Can build urls for Kartverket general and Electron

---
+++ WebSvc/Controllers/MapController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+using MoreMapsTileFetcher;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebSvc.Controllers
+{
+    [Route("api/[controller]")]
+    public class MapController : Controller
+    {
+        internal string buildKartverketURLString(string mapKey)
+        {
+            string _wmsCacheKartverketUrlHead = @"http://opencache3.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=";
+            string _wmsCacheKartverketUrlTail = @"&zoom={0}&x={1}&y={2}";
+            string theWMSUrl = _wmsCacheKartverketUrlHead + mapKey + _wmsCacheKartverketUrlTail;
+            return theWMSUrl;
+        }
+
+        public static string GetElektronTicket()
+        {
+            string theTicket = "empty";
+            string url = "http://beta.norgeskart.no/ws/esk.py?wms.ecc_enc";
+
+            var task = MakeAsyncRequest(url, "text/html");
+            theTicket = task.Result;
+
+            var trimmedTicket = theTicket.Trim('\"', '\n');
+
+            return trimmedTicket;
+        }
+
+        public static Task<string> MakeAsyncRequest(string url, string contentType)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = contentType;
+            //request.Method = WebRequestMethods.Http.Get;
+            //request.Timeout = 20000;
+            request.Proxy = null;
+
+            Task<WebResponse> task = Task.Factory.FromAsync(
+                request.BeginGetResponse,
+                asyncResult => request.EndGetResponse(asyncResult),
+                (object)null);
+
+            return task.ContinueWith(t => ReadStreamFromResponse(t.Result));
+        }
+
+        private static string ReadStreamFromResponse(WebResponse response)
+        {
+            using (Stream responseStream = response.GetResponseStream())
+            using (StreamReader sr = new StreamReader(responseStream))
+            {
+                //Need to return this response 
+                string strContent = sr.ReadToEnd();
+                return strContent;
+            }
+        }
+
+        public static string BuildElektronUrl(int x, int y, int zoom, string theTicket)
+        {
+            string url = "";
+
+            var theTileBounds = GlobalMercator.TileBounds(new GlobalMercator.Tile(x, y), zoom);
+            string urlBase = "http://wms.geonorge.no/skwms1/wms.ecc_enc?LAYERS=cells&STYLES=style-id-263&FORMAT=image%2Fpng&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG%3A900913";
+            string urlBBox = "&BBOX=" + theTileBounds.Left.ToString(CultureInfo.InvariantCulture) + ","
+                                        + theTileBounds.Bottom.ToString(CultureInfo.InvariantCulture) + ","
+                                        + theTileBounds.Right.ToString(CultureInfo.InvariantCulture) + ","
+                                        + theTileBounds.Top.ToString(CultureInfo.InvariantCulture);
+            string urlTileSize = "&WIDTH=256&HEIGHT=256";
+            string urlTicket = "&ticket=" + theTicket;
+            url = urlBase + urlBBox + urlTileSize + urlTicket;
+            return url;
+        }
+
+        private const string defName = "empty";
+        private const int defX = -1;
+        private const int defY = -1;
+        private const int defZoom = -1;
+        // GET api/map?X=xvalue....
+        [HttpGet]
+        public string Get(string name = defName, int x = defX, int y = defY, int zoom = defZoom)
+        {
+            // Look here for mental model http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/
+            //int x = 69;
+            //int y = 29;
+            //int zoom = 7;
+
+            // Andenes havn
+            // x =4462
+            // y =1877
+            // zoom=13
+
+            if (name == defName || x == defX || y == defY || zoom == defZoom)
+            {
+                var helpful = string.Format("Try this: http://localhost:33005/api/map?name={0}&x={1}&y={2}&zoom={3}", "Electron", 4462, 1877, 13);
+                return helpful;
+            }
+            else
+            {
+                var dill = string.Format("Hello! Will now fetch {0} map with coordinates {1}/{2}/{3}", name, x, y, zoom);
+                string electronUrl = BuildElektronUrl(x, y, zoom, GetElektronTicket());
+                string conventionalKartverket = string.Format(buildKartverketURLString("topo2"), zoom, x, y);
+                dill = dill + "\r\n\r\n" + electronUrl + "\r\n\r\n" + conventionalKartverket;
+
+                return dill;
+            }
+        }
+    }
+}
---
+++ WebSvc/GlobalMercator.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+
+namespace MoreMapsTileFetcher
+{
+    /// <summary>
+    /// Conversion routines for Google, TMS, and Microsoft Quadtree tile representations, derived from
+    /// http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/ 
+    /// </summary>
+    public class GlobalMercator
+    {
+        private const int TileSize = 256;
+        private const int EarthRadius = 6378137;
+        private const double InitialResolution = 2 * Math.PI * EarthRadius / TileSize;
+        private const double OriginShift = 2 * Math.PI * EarthRadius / 2;
+
+
+        /// <summary>
+        /// A two dimensional point in space
+        /// </summary>
+        public struct Point
+        {
+            public Point(double x, double y) : this()
+            {
+                X = x;
+                Y = y;
+            }
+
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+
+        /// <summary>
+        /// Reference to a Tile X, Y index
+        /// </summary>
+        public class Tile
+        {
+            public Tile() { }
+            public Tile(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        //Resolution (meters/pixel) for given zoom level (measured at Equator)
+        public static double Resolution(int zoom)
+        {
+            return InitialResolution / (Math.Pow(2, zoom));
+        }
+
+        //Converts pixel coordinates in given zoom level of pyramid to EPSG:900913
+        public static Point PixelsToMeters(Point p, int zoom)
+        {
+            // Convert to meters
+            var res = Resolution(zoom);
+            var meters = new Point();
+            meters.X = p.X * res - OriginShift;
+            meters.Y = p.Y * res;// - OriginShift;
+                        
+            // The abstraction of Google having origo top left and BBOX having origo center need to go here
+            //meters.Y = 2 * Math.PI * EarthRadius - meters.Y;
+            meters.Y = meters.Y - OriginShift;
+
+            return meters;
+        }
+
+        //Returns bounds of the given tile in EPSG:900913 coordinates
+        public static Rect TileBounds(Tile t, int zoom)
+        {
+            // Convert from origo top left to origo bottom left
+            t.Y = (int) Math.Pow(2, zoom) - 1 - t.Y;
+
+            var min = PixelsToMeters(new Point(t.X * TileSize, t.Y * TileSize), zoom);
+            var max = PixelsToMeters(new Point((t.X + 1) * TileSize, (t.Y + 1) * TileSize), zoom);
+
+            return new Rect(min, max);
+        }
+
+        /// <summary>
+        /// A rectangle defined by top left and bottom right points. Origo bottom left.
+        /// </summary>
+        public struct Rect
+        {
+            public Rect(Point p1, Point p2) : this()
+            {
+                P1 = p1;
+                P2 = p2;
+
+                // The Google maps coordinate system works with upper (north) left as origo
+                Left = P1.X;
+                Right = P2.X;
+                Top = P2.Y;
+                Bottom = P1.Y;
+            }
+
+            // NB: Google tile system is top left (x,y) = (0,0)
+            public double Left { get; internal set; }
+            public double Right { get; internal set; }
+            public double Top { get; internal set; }
+            public double Bottom { get; internal set; }
+
+            public Point P1 { get; set; }
+            public Point P2 { get; set; }
+
+        }
+    }
+}
